be5c3955495ee5ecdb7a502ace46bb2d4ac46e31
Palash Rambhia <palashrambhia2@gmail.com>
2023-02-13 22:06:28 +0530

    Firebase Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:aikyam/demoScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
