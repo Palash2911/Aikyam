4ad79c3184fdca2c2e8a49e822cb9f68bc638625
DipHire <diphire111@gmail.com>
2023-02-25 15:15:07 +0530

    ngo: home

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:aikyam/views/Screens/Ngo/NActivityScreen.dart';
+import 'package:aikyam/views/Screens/Ngo/NAddPostScreen.dart';
+import 'package:aikyam/views/Screens/Ngo/NHomeScreen.dart';
+import 'package:aikyam/views/Screens/Ngo/NchatScreen.dart';
 import 'package:aikyam/views/Screens/User/ActivityScreen.dart';
 import 'package:aikyam/views/Screens/User/ChatScreen.dart';
-import 'package:aikyam/views/Screens/User/ChatScreenOpen.dart';
 import 'package:aikyam/views/Screens/User/HomeScreen.dart';
 import 'package:aikyam/views/constants.dart';
 import 'package:aikyam/views/widgets/BottomNavBar.dart';
+import 'package:aikyam/views/widgets/ngoBottomBar.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -21,10 +25,20 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       theme: aikyamTheme,
       routes: {
-        '/': (context) => CustomBottomBar(),
-        HomeScreen.routeName: (context) => HomeScreen(),
-        ChatScreen.routeName: (context) => ChatScreen(),
-        ActivityScreen.routeName: (context) => ActivityScreen(),
+        //?User Routes
+
+        // '/': (context) => UserBottomBar(),
+        // HomeScreen.routeName: (context) => HomeScreen(),
+        // ChatScreen.routeName: (context) => ChatScreen(),
+        // ActivityScreen.routeName: (context) => ActivityScreen(),
+
+
+        //?Ngo Routes
+         '/': (context) => NgoBottomBar(),
+        NHomeScreen.routeName: (context) => NHomeScreen(),
+        NgoChatScreen.routeName: (context) => NgoChatScreen(),
+        NgoAddpost.routeName: (context) => NgoAddpost(),
+        NgoActivityScreen.routeName: (context) => NgoActivityScreen(),
       },
     );
   }
