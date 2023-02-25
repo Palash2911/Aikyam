a960bd046fc711f8e99d7b8b604321c754b4e119
DipHire <diphire111@gmail.com>
2023-02-25 15:14:52 +0530

    ngo: add post,activity ,chat

---
+++ lib/views/Screens/Ngo/NAddPostScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class NgoAddpost extends StatefulWidget {
+  const NgoAddpost({super.key});
+
+    static const routeName = '/ngo_chat_screen';
+
+
+  @override
+  State<NgoAddpost> createState() => _NgoAddpostState();
+}
+
+class _NgoAddpostState extends State<NgoAddpost> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/views/Screens/Ngo/NchatScreen.dart
@@ -0,0 +1,145 @@
+import 'package:aikyam/views/Screens/User/ChatScreenOpen.dart';
+import 'package:aikyam/views/widgets/roundAppBar.dart';
+import 'package:flutter/material.dart';
+import '../../widgets/chatUser.dart';
+
+class NgoChatScreen extends StatelessWidget {
+  const NgoChatScreen({super.key});
+
+  static const routeName = '/chat_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          toolbarHeight: 85,
+          flexibleSpace: RoundAppBar(
+            title: 'Chat',
+          ),
+        ),
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: ListView(
+              children: [
+                ChatListItem(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ChatScreenOpen()),
+                    );
+                  },
+                  name: 'Smile Foundation',
+                  message: 'Hii I would like to ..',
+                  isOnline: true,
+                  imageUrl: 'assets/images/ngo.png',
+                ),
+                ChatListItem(
+                  onTap: () {},
+                  name: 'Dip',
+                  message: 'Its 4 am im doin this shit',
+                  isOnline: false,
+                  imageUrl: 'assets/images/profile.png',
+                ),
+                ChatListItem(
+                  onTap: () {},
+                  name: 'Smile Foundation',
+                  message: 'Hii I would like to ..',
+                  isOnline: true,
+                  imageUrl: 'assets/images/ngo.png',
+                ),
+                ChatListItem(
+                  onTap: () {},
+                  name: 'Dip',
+                  message: 'Its 4 am im doin this shit',
+                  isOnline: false,
+                  imageUrl: 'assets/images/profile.png',
+                ),
+                ChatListItem(
+                  onTap: () {},
+                  name: 'Dip',
+                  message: 'Its 4 am im doin this shit',
+                  isOnline: false,
+                  imageUrl: 'assets/images/profile.png',
+                ),
+                ChatListItem(
+                  onTap: () {},
+                  name: 'Dip',
+                  message: 'Its 4 am im doin this shit',
+                  isOnline: false,
+                  imageUrl: 'assets/images/profile.png',
+                ),
+                ChatListItem(
+                  onTap: () {},
+                  name: 'Elon Musk',
+                  message: '',
+                  isOnline: false,
+                  imageUrl: 'assets/images/profile.png',
+                ),
+                ChatListItem(
+                  onTap: () {},
+                  name: 'Smile Foundation',
+                  message: 'Hii I would like to ..',
+                  isOnline: true,
+                  imageUrl: 'assets/images/ngo.png',
+                ),
+                ChatListItem(
+                  onTap: () {},
+                  name: 'Smile Foundation',
+                  message: 'Hii I would like to ..',
+                  isOnline: true,
+                  imageUrl: 'assets/images/ngo.png',
+                ),
+                ChatListItem(
+                  onTap: () {},
+                  name: 'Smile Foundation',
+                  message: 'Hii I would like to ..',
+                  isOnline: true,
+                  imageUrl: 'assets/images/ngo.png',
+                ),
+                ChatListItem(
+                  onTap: () {},
+                  name: 'Smile Foundation',
+                  message: 'Hii I would like to ..',
+                  isOnline: true,
+                  imageUrl: 'assets/images/ngo.png',
+                ),
+                ChatListItem(
+                  onTap: () {},
+                  name: 'Smile Foundation',
+                  message: 'Hii I would like to ..',
+                  isOnline: true,
+                  imageUrl: 'assets/images/dp.jpg',
+                ),
+                ChatListItem(
+                  onTap: () {},
+                  name: 'Smile Foundation',
+                  message: 'Hii I would like to ..',
+                  isOnline: true,
+                  imageUrl: 'assets/images/dp.jpg',
+                ),
+                ChatListItem(
+                  onTap: () {},
+                  name: 'Smile Foundation',
+                  message: 'Hii I would like to ..',
+                  isOnline: true,
+                  imageUrl: 'assets/images/dp.jpg',
+                ),
+                ChatListItem(
+                  onTap: () {},
+                  name: 'Elon Musk',
+                  message: '',
+                  isOnline: false,
+                  imageUrl: 'assets/images/profile.png',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
