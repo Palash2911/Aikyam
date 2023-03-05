9c2826a5ac6d497599f86055c6a375a89ea9eaaa
Palash Rambhia <palashrambhia2@gmail.com>
2023-03-05 23:11:12 +0530

    UI Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,9 @@
-import 'package:aikyam/views/Screens/Ngo/Login.dart';
-import 'package:aikyam/views/Screens/Ngo/NActivityScreen.dart';
-import 'package:aikyam/views/Screens/Ngo/NAddPostScreen.dart';
-import 'package:aikyam/views/Screens/Ngo/NHomeScreen.dart';
-import 'package:aikyam/views/Screens/Ngo/NchatScreen.dart';
-import 'package:aikyam/views/Screens/Ngo/OTP.dart';
-import 'package:aikyam/views/Screens/Ngo/SplashScreen.dart';
-import 'package:aikyam/views/Screens/Ngo/Intro.dart';
 import 'package:aikyam/views/Screens/User/ActivityScreen.dart';
 import 'package:aikyam/views/Screens/User/ChatScreen.dart';
 import 'package:aikyam/views/Screens/User/HomeScreen.dart';
 import 'package:aikyam/views/constants.dart';
-import 'package:aikyam/views/widgets/BottomNavBar.dart';
-import 'package:aikyam/views/widgets/ngoBottomBar.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:intro_slider/intro_slider.dart';
-import 'package:lottie/lottie.dart';
 import 'package:aikyam/views/Screens/Ngo/Logo.dart';
 
 void main() async {
