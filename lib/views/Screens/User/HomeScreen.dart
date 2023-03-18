85a7f34a750c0d8e3455a7798f3e648f8479de08
Palash Rambhia <palashrambhia2@gmail.com>
2023-03-19 00:00:22 +0530

    Sakshi Dev Merged

--- lib/views/Screens/User/HomeScreen.dart
+++ lib/views/Screens/User/HomeScreen.dart
@@ -2,6 +2,7 @@ import 'package:aikyam/providers/user_provider.dart';
 import 'package:aikyam/views/widgets/AppBarHome.dart';
 import 'package:aikyam/views/widgets/AppDrawer.dart';
 import 'package:aikyam/views/widgets/Post.dart';
+import 'package:aikyam/views/widgets/Post2.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -36,21 +37,52 @@ class _HomeScreenState extends State<HomeScreen> {
             onPressed: () {
               _scaffoldKey.currentState?.openDrawer();
             },
-            icon: CircleAvatar(
+            icon: const CircleAvatar(
               radius: 25.0,
               backgroundImage: AssetImage('assets/images/dp.jpg'),
             ),
           ),
         ),
-        drawer: UserAppdrawer(),
+        drawer: const UserAppdrawer(),
         body: SingleChildScrollView(
           child: Column(
-            children: [
-              Post(),
-              Post(),
-              Post(),
-              Post(),
-              Post(),
+            children: const [
+              PostItem(
+                ngoname: '',
+                drivecity: '',
+                driveDate: '',
+                ngocity: '',
+                driveaddress: '',
+                applyStatus: '',
+                pid: '',
+              ),
+              PostItem(
+                ngoname: '',
+                drivecity: '',
+                driveDate: '',
+                ngocity: '',
+                driveaddress: '',
+                applyStatus: '',
+                pid: '',
+              ),
+              PostItem(
+                ngoname: '',
+                drivecity: '',
+                driveDate: '',
+                ngocity: '',
+                driveaddress: '',
+                applyStatus: '',
+                pid: '',
+              ),
+              PostItem(
+                ngoname: '',
+                drivecity: '',
+                driveDate: '',
+                ngocity: '',
+                driveaddress: '',
+                applyStatus: '',
+                pid: '',
+              ),
             ],
           ),
         ),
